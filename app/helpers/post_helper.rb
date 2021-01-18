1e8089d228a10a9f2c5abc281ece78033fa800fa
Nexch <mauricio.t.1999@gmail.com>
2021-01-18 02:56:36 -0500

    add TSE features

--- app/helpers/post_helper.rb
+++ app/helpers/post_helper.rb
@@ -4,4 +4,12 @@ module PostHelper
 
     content_tag :p, "Post could not be saved. #{post.errors.full_messages.join('. ')}", class: 'errors'
   end
+
+  def if_friend(lol, lists)
+    if (lists.include?(lol.user_id))
+      render lol
+    else
+    end
+
+  end
 end
